96238c441e4d7e6d8087d6935a36fdfca42034c0
Youth-o <2021111495@dgu.ac.kr>
2025-01-22 12:03:39 +0900

    initial commit

---
+++ server/hr-master/src/main/java/com/example/hr_master/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.hr_master.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/showMe")
+    public List<String> hello() {
+        return Arrays.asList("첫번째 인사", "두번째 인사");
+    }
+}
