0ddee1f9e9658aba4d4d4f6c656581af3ad7ba8e
Youth-o <2021111495@dgu.ac.kr>
2025-02-05 11:30:55 +0900

    style: module css로 변경

--- frontend/hr-master/src/components/Login/LoginAside.jsx
+++ frontend/hr-master/src/components/Login/LoginAside.jsx
@@ -1,17 +1,17 @@
-import './LoginAside.css';
+import styles from './LoginAside.module.css';
 
 export function LoginAside() {
 	return (
-		<div class="login">
-			<div class="left-content">
-				<div class="bg"></div>
-				<div class="ellipse-1"></div>
-				<div class="ellipse-2"></div>
-				<div class="container">
-					<div class="title">서한그룹 인사 관리 시스템</div>
-					<div class="title-eng">Seohan HR Master</div>
-					<div class="frame-3">
-						<div class="read-more">Read More</div>
+		<div className={styles.login}>
+			<div className={styles.leftContent}>
+				<div className={styles.bg}></div>
+				<div className={styles.eclipse1}></div>
+				<div className={styles.eclipse2}></div>
+				<div className={styles.container}>
+					<div className={styles.title}>서한그룹 인사 관리 시스템</div>
+					<div className={styles.titleEng}>Seohan HR Master</div>
+					<div className={styles.frame}>
+						<div className={styles.readMore}>Read More</div>
 					</div>
 				</div>
 			</div>
