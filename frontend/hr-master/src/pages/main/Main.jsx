e162c407d77c83190f883ae2d725c600d5ddcf79
Youth-o <2021111495@dgu.ac.kr>
2025-02-06 15:11:11 +0900

    feat: Nav UI 완성

--- frontend/hr-master/src/pages/main/Main.jsx
+++ frontend/hr-master/src/pages/main/Main.jsx
@@ -1,5 +1,11 @@
 import Header from '../../components/common/Header/Header';
+import Nav from '../../components/common/Nav/Nav';
 
 export default function Main() {
-	return <Header></Header>;
+	return (
+		<div>
+			<Header />
+			<Nav />
+		</div>
+	);
 }
