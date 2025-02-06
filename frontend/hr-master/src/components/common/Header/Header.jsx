81d9ebd17e002d5d031c5cbb8dacb69a6181abf8
Youth-o <2021111495@dgu.ac.kr>
2025-02-06 14:11:44 +0900

    feat: Search 컴포넌트 개발 및 Header UI 완성

--- frontend/hr-master/src/components/common/Header/Header.jsx
+++ frontend/hr-master/src/components/common/Header/Header.jsx
@@ -1,5 +1,6 @@
 import styles from './Header.module.css';
 import logo from '../../../assets/logo.svg';
+import Search from '../Search/Search';
 
 export default function Header() {
 	return (
@@ -7,6 +8,10 @@ export default function Header() {
 			<div className={styles.logoBox}>
 				<img src={logo} alt="로고" />
 				<p className={styles.logoText}>서한 인사관리 시스템</p>
+			</div>
+			<div className={styles.searchBox}>
+				<Search />
+				<button>로그아웃</button>
 			</div>
 		</header>
 	);
