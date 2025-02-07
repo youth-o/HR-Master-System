020215bcd1fe34c3977d24c68222bc1749b962e6
Youth-o <2021111495@dgu.ac.kr>
2025-02-07 14:23:28 +0900

    feat: Nav에서 사원 조회 및 수정 클릭시 /employees로 이동

--- frontend/hr-master/src/components/EmployeesList/List/List.jsx
+++ frontend/hr-master/src/components/EmployeesList/List/List.jsx
@@ -9,18 +9,25 @@ export default function List() {
 
 	return (
 		<div className={styles.listContainer}>
-			<h2>Employee List</h2>
-			<ul>
-				{employees.length > 0 ? (
-					employees.map((employee) => (
-						<li key={employee.empId}>
-							{employee.empName} - {employee.position}
-						</li>
-					))
-				) : (
-					<p>No employees found.</p>
-				)}
-			</ul>
+			<h3>전체 사원 조회</h3>
+			<div className={styles.table}>
+				<p>사번</p>
+				<p>이름</p>
+				<p>부서</p>
+				<p>사내 메일</p>
+			</div>
+			{employees.length > 0 ? (
+				employees.map((employee) => (
+					<div className={styles.listBox} key={employee.employeeId}>
+						<p>{employee.employeeId}</p>
+						<p>{employee.empName}</p>
+						<p>{employee.department}</p>
+						<p>{employee.companyEmail}</p>
+					</div>
+				))
+			) : (
+				<p>No employees found.</p>
+			)}
 		</div>
 	);
 }
