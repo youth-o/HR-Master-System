8e51cd8b282b273acdf776242265d8c293a2d0f5
Youth-o <2021111495@dgu.ac.kr>
2025-02-07 14:36:13 +0900

    feat: 로고 클릭시 /main으로 이동

--- frontend/hr-master/src/components/common/Header/Header.jsx
+++ frontend/hr-master/src/components/common/Header/Header.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../../../assets/logo.svg';
 import Search from '../Search/Search';
 
 export default function Header() {
+	const navigate = useNavigate();
+	const handleLogoClick = (path) => {
+		if (path) {
+			navigate(path);
+		}
+	};
+
 	return (
 		<header>
-			<div className={styles.logoBox}>
+			<div className={styles.logoBox} onClick={() => handleLogoClick('/main')}>
 				<img src={logo} alt="로고" />
 				<p className={styles.logoText}>서한 인사관리 시스템</p>
 			</div>
