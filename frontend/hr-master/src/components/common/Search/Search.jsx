e549ca10b11ed833b87d734365bcf0cdf998d18a
Youth-o <2021111495@dgu.ac.kr>
2025-02-08 21:56:59 +0900

    feat: 전체 사원 리스트에서 사원 검색 기능 추가

--- frontend/hr-master/src/components/common/Search/Search.jsx
+++ frontend/hr-master/src/components/common/Search/Search.jsx
@@ -6,11 +6,19 @@ export default function Search({
 	style = {},
 	containerStyle = {},
 	placeholderColor = ' #757575',
+	onChange = () => {},
+	onSearch = () => {},
 }) {
 	return (
 		<div className={styles.searchContainer} style={containerStyle}>
-			<img src={search} alt="검색아이콘" className={styles.searchIcon} />
-			<input placeholder={placeholder} type="text" style={style} placeholderColor={placeholderColor} />
+			<img src={search} alt="검색아이콘" className={styles.searchIcon} onClick={onSearch} />
+			<input
+				placeholder={placeholder}
+				type="text"
+				style={style}
+				placeholderColor={placeholderColor}
+				onChange={onChange}
+			/>
 		</div>
 	);
 }
