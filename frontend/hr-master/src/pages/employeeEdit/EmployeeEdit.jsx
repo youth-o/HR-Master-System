6cf3f3721dc8e88142a62379dc0f74b41ba53a5e
Youth-o <2021111495@dgu.ac.kr>
2025-02-10 15:14:37 +0900

    style: nav 스타일 수정

--- frontend/hr-master/src/pages/employeeEdit/EmployeeEdit.jsx
+++ frontend/hr-master/src/pages/employeeEdit/EmployeeEdit.jsx
@@ -5,14 +5,14 @@ import Nav from '../../components/common/Nav/Nav';
 
 export default function EmployeeEdit() {
 	return (
-		<>
+		<div className={styles.employeeEditContainer}>
 			<Header />
-			<div className={styles.layout}>
+			<div className={styles.content}>
 				<Nav />
 				<div className={styles.employeeEdit}>
 					<EmployeeMenu />
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
