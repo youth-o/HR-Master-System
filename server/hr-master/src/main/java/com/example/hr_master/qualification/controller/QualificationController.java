d37b3b6588ce7391f5da3ca71247ae1adf658899
Youth-o <2021111495@dgu.ac.kr>
2025-02-10 16:01:48 +0900

    .

--- server/hr-master/src/main/java/com/example/hr_master/qualification/controller/QualificationController.java
+++ server/hr-master/src/main/java/com/example/hr_master/qualification/controller/QualificationController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-dev
 public class QualificationController {
     private final QualificationService qualificationService;
 
@@ -17,33 +16,28 @@ public class QualificationController {
     }
 
     // 전체 employee_id의 자격 사항 조회
- dev
     public ResponseEntity<List<Qualification>> getAllQualifications() {
         return ResponseEntity.ok(qualificationService.getAllQualifications());
     }
 
     // 특정 employee_id의 자격 사항 조회
-dev
     public ResponseEntity<List<Qualification>> getQualificationsByEmployeeId(@PathVariable Long employeeId) {
         return ResponseEntity.ok(qualificationService.getQualificationsByEmployeeId(employeeId));
     }
 
     // 특정 employee_id의 자격 사항 추가
- dev
     public ResponseEntity<Qualification> addQualification(@PathVariable Long employeeId,
                                                           @RequestBody Qualification qualification) {
         return ResponseEntity.ok(qualificationService.addQualification(employeeId, qualification));
     }
 
     // 특정 자격 사항 수정 (수정 기능 추가)
-dev
     public ResponseEntity<Qualification> updateQualification(@PathVariable Long qualificationId,
                                                              @RequestBody Qualification qualification) {
         return ResponseEntity.ok(qualificationService.updateQualification(qualificationId, qualification));
     }
 
     // 특정 자격 사항 삭제
-dev
     public ResponseEntity<Void> deleteQualification(@PathVariable Long qualificationId) {
         qualificationService.deleteQualification(qualificationId);
         return ResponseEntity.noContent().build();
