7c33bed1b52305e0242eb37d2da57d40ce892b5d
kimzooci <kjhvkwn@gmail.com>
2025-02-10 11:32:20 +0900

    Calendar UI 업데이트

--- frontend/hr-master/src/App.js
+++ frontend/hr-master/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 // import EmployeesList from './pages/employeesList/EmployeesList';
 // import EmployeeEdit from './pages/employeeEdit/EmployeeEdit';
 // import Dashboard from './pages/Dashboard/Dashboard';
-import Message from './pages/Message/Message'
+// import Message from './pages/Message/Message'
+import Calendar from'./pages/Calendar/Calendar'
 
 
 // 로그인 여부 확인 함수
@@ -23,7 +24,7 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				{/* 첫 화면을 로그인 페이지로 설정 */}
-				<Route path="/" element={<Message />} />
+				<Route path="/" element={<Calendar />} />
 
 				{/* 로그인된 사용자만 접근 가능 */}
 				{/* 
