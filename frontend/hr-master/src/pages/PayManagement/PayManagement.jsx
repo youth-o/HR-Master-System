bfee5b4428c28f95c9a91eb77eb46d9ff83e5637
Youth-o <2021111495@dgu.ac.kr>
2025-02-11 18:19:36 +0900

    style: 오타 수정

--- frontend/hr-master/src/pages/PayManagement/PayManagement.jsx
+++ frontend/hr-master/src/pages/PayManagement/PayManagement.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 import Header from '../../components/common/Header/Header';
 import Nav from '../../components/common/Nav/Nav';
 
-import EmployeeInfo from '../../components/\bPayManagement/EmployeePayInfo';
-import UpcomingPay from '../../components/\bPayManagement/UpcomingPay';
-import PayDetails from '../../components/\bPayManagement/PayDetails';
-import PayrollSummary from '../../components/\bPayManagement/PayrollSummary';
+import EmployeeInfo from '../../components/PayManagement/EmployeePayInfo';
+import UpcomingPay from '../../components/PayManagement/UpcomingPay';
+import PayDetails from '../../components/PayManagement/PayDetails';
+import PayrollSummary from '../../components/PayManagement/PayrollSummary';
 
 import './PayManagement.css';
 
 const PayManagement = () => {
-    return (
-        <>
-            <Header />
-            <div className="pay-management-layout">
-                <Nav />
-                <div className="pay-management-content">
-                    <EmployeeInfo />
-                    <div className="pay-summary-section">
-                        <UpcomingPay />
-                        <PayDetails />
-                    </div>
-                    <PayrollSummary />
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<Header />
+			<div className="pay-management-layout">
+				<Nav />
+				<div className="pay-management-content">
+					<EmployeeInfo />
+					<div className="pay-summary-section">
+						<UpcomingPay />
+						<PayDetails />
+					</div>
+					<PayrollSummary />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default PayManagement;
