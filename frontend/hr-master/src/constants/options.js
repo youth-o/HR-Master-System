832f33e7d192258c306d00fa24e2af5f13835e38
Youth-o <2021111495@dgu.ac.kr>
2025-02-11 16:36:26 +0900

    feat: 사원 등록 API 추가

--- frontend/hr-master/src/constants/options.js
+++ frontend/hr-master/src/constants/options.js
@@ -1,5 +1,7 @@
+export const genderOptions = ['남', '여'];
 export const hireOptions = ['신입', '경력'];
-export const militaryOptions = ['군필', '미필'];
+export const militaryOptions = ['군필', '미필', '해당없음'];
+export const retireClsOptions = ['재직', '퇴직'];
 export const workLocationOptions = ['서한ENP', '기획본부', '서한이노빌리티', '한국무브넥스', '캄텍'];
 export const departmentOptions = ['인사', 'IT기획', '품질', '공정기술', '안전', '생산관리', '영업', '협력업체관리'];
 export const positionOptions = ['인턴', '사원', '주임', '대리', '과장', '차장', '부장', '이사', '사장'];
