ec05e01eb1b4ade48cd41f68ae7b29c92a94a093
Youth-o <2021111495@dgu.ac.kr>
2025-02-11 15:26:57 +0900

    feat: Dropdown 컴포넌트 추가 및 적용

--- frontend/hr-master/src/components/Register/RegisterForm.jsx
+++ frontend/hr-master/src/components/Register/RegisterForm.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react';
 import Input from '../common/Input/Input';
+import Dropdown from '../common/Dropdown/Dropdown';
+import {
+	hireOptions,
+	militaryOptions,
+	workLocationOptions,
+	departmentOptions,
+	positionOptions,
+} from '../../constants/options';
 import './RegisterForm.css';
 
 export default function RegisterForm() {
-	const [hireType, setHireType] = useState('');
+	const [formData, setFormData] = useState({
+		hireType: '',
+		military: '',
+		workLocation: '',
+		department: '',
+		position: '',
+	});
 
-	const handleHireTypeChange = (e) => {
-		setHireType(e.target.value);
+	const handleDropdownChange = (field, selected) => {
+		setFormData((prev) => ({
+			...prev,
+			[field]: selected,
+		}));
 	};
 
 	const style = {
@@ -17,7 +34,6 @@ export default function RegisterForm() {
 		<div className="infoContainer">
 			<form className="infoForm">
 				<h3>개인정보</h3>
-				{/* 개인 정보 */}
 				<div className="row">
 					<Input id="employeeId" label="사번(ID)" />
 					<Input id="ssn" label="주민번호" />
@@ -26,22 +42,43 @@ export default function RegisterForm() {
 				<div className="row">
 					<Input id="empName" label="이름" />
 					<Input id="empEngName" label="영문 이름" />
-					<Input id="hireDate" label="입사일" />
+					<Input id="hireDate" type="date" label="입사일" />
 				</div>
 				<div className="row">
 					<Input id="nationality" label="국적" />
-					<Input id="militaryService" label="군필 여부" />
+					<Dropdown
+						label="군필 여부"
+						menuItems={militaryOptions}
+						onSelect={(val) => handleDropdownChange('military', val)}
+					/>
 					<Input id="address" label="주소" />
 				</div>
+
 				{/* 근무 정보 */}
 				<h3>근무정보</h3>
 				<div className="row">
-					<Input id="hireType" label="입사 구분" onChange={handleHireTypeChange} />
-					<Input id="workLocation" label="근무지" />
-					<Input id="department" label="부서" />
+					<Dropdown
+						label="입사 구분"
+						menuItems={hireOptions}
+						onSelect={(val) => handleDropdownChange('hireType', val)}
+					/>
+					<Dropdown
+						label="근무지"
+						menuItems={workLocationOptions}
+						onSelect={(val) => handleDropdownChange('workLocation', val)}
+					/>
+					<Dropdown
+						label="부서"
+						menuItems={departmentOptions}
+						onSelect={(val) => handleDropdownChange('department', val)}
+					/>
 				</div>
 				<div className="row">
-					<Input id="hireType" label="직급" />
+					<Dropdown
+						label="직급"
+						menuItems={positionOptions}
+						onSelect={(val) => handleDropdownChange('position', val)}
+					/>
 					<Input id="companyWork" label="회사 근무 사항" />
 					<Input id="evaluationFlag" label="고과 여부" />
 				</div>
@@ -51,7 +88,7 @@ export default function RegisterForm() {
 				</div>
 
 				{/* 사외 경력 정보 */}
-				{hireType === '경력' && (
+				{formData.hireType === '경력' && (
 					<>
 						<h3>사외 경력</h3>
 						<div className="row">
@@ -67,7 +104,6 @@ export default function RegisterForm() {
 					</>
 				)}
 
-				{/* 버튼 */}
 				<button type="submit">Add Staff</button>
 			</form>
 		</div>
