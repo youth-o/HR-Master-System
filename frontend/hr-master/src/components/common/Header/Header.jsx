f0edceb1f29d1d0bf0d0b9a1bd8259cf63da66a4
kiyuniii <bkyun3012@sju.ac.kr>
2025-02-14 13:30:35 +0900

    feat(Add): 로그인 및 로그아웃 기능 추가

--- frontend/hr-master/src/components/common/Header/Header.jsx
+++ frontend/hr-master/src/components/common/Header/Header.jsx
@@ -2,12 +2,24 @@ import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../../../assets/logo.svg';
 import Search from '../Search/Search';
+import { logout } from "../../../apis/Logout";
 
 export default function Header() {
 	const navigate = useNavigate();
 	const handleLogoClick = (path) => {
 		if (path) {
 			navigate(path);
+		}
+	};
+
+	const handleLogout = async () => {
+		const result = await logout();
+		if (result.success) {
+			localStorage.removeItem("token"); // JWT 사용 시 로컬 스토리지에서 토큰 삭제
+			sessionStorage.clear(); // 세션 스토리지도 정리
+			navigate("/"); // 로그인 페이지로 이동
+		} else {
+			alert("로그아웃 실패!");
 		}
 	};
 
@@ -19,7 +31,7 @@ export default function Header() {
 			</div>
 			<div className={styles.searchBox}>
 				<Search />
-				<button>로그아웃</button>
+				<button onClick={handleLogout}>로그아웃</button>
 			</div>
 		</header>
 	);
